ee32675b95fc22dc048f2cd0019f4f4d90c980d2
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-04-12 11:12:21 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<script setup>
+import LightBulb from './components/LightBulb.vue';
+</script>
+
+<script>
+  export default {
+    data() {
+      return {
+        clickRange: 5,
+        totalBulbs: 5,
+        bulbsSwitchedOn: 0,
+        allOn: false
+      }
+    },
+    methods: {
+      onBulbSwitchedOn() {
+        console.log("ensendida")
+        this.bulbsSwitchedOn++;
+
+        if (this.bulbsSwitchedOn == this.totalBulbs) {
+          this.allOn = true;
+        }
+      },
+      getRandomClickRange() {
+        return Math.floor(Math.random() * this.clickRange) + 1;
+      }
+    }
+  }
+</script>
+
+<template>
+  <main>
+    <div>
+      <h1 v-if="allOn">Todas las bombillas están encendidas!</h1>
+      <h1 v-else>Faltan bombillas por encender!</h1>
+    </div>
+    <div class="bulbs">
+      <LightBulb v-for="n in totalBulbs" :key="n" :neededClicks="getRandomClickRange()" @switchedOn="onBulbSwitchedOn"/>
+    </div>
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+}
+
+.bulbs {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+h1{
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+}
+</style>
---
+++ src/components/LightBulb.vue
@@ -0,0 +1,49 @@
+<script>
+    export default {
+        props: ["neededClicks"],
+        emits: ["switchedOn"],
+        data() {
+            return {
+                isOn: false,
+                clicks: +this.neededClicks
+            }
+        },
+        methods: {
+            onClick() {
+                this.clicks--;
+                
+                if (this.clicks == 0) {
+                    this.isOn = !this.isOn;
+                    this.$emit("switchedOn");
+                }
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="bulb" :class="isOn ? 'on' : 'off'" @click="(!isOn) ? onClick() : ''">
+        <h2 v-if="!isOn">{{ clicks }}</h2>
+    </div>
+</template>
+    
+<style scoped>
+    .bulb {
+        width: 265px;
+        height: 400px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 1.5rem;
+        cursor: pointer;
+        user-select: none;
+    }
+
+    .bulb.on {
+        background-image: url('lighton.png');
+    }
+
+    .bulb.off {
+        background-image: url('lightoff.png');
+    }
+</style>
